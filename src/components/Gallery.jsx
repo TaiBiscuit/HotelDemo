91369a6f602fecd6a30fb7c0a9fa829920433f85
nicolc <nicolas.l.campos1@gmail.com>
2024-04-09 01:02:12 -0300

    betteImg

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -49,12 +49,12 @@ export const Gallery = () => {
         <div className="gallery-wrap-mobile">
             <div className="gallery-carrousel">
                 <div className="slide fade"><div className="gallery-img-container galimgmob"><img src="https://i.ibb.co/3CY1QVB/hotel3.jpg" alt="" className="gallery-img-m" /></div></div>
-                <div className="slide fade"><div className="gallery-img-container2 galimgmob"><img src="https://i.ibb.co/NTyR4yF/hotel7.jpg" alt="" className="gallery-img-m"/></div></div>
-                <div className="slide fade"><div className="gallery-img-container3 galimgmob"><img src="https://i.ibb.co/8Yw8shC/hotel8.jpg" alt="" className="gallery-img-m"/></div></div>
-                <div className="slide fade"><div className="gallery-img-container5 galimgmob"><img src="https://i.ibb.co/frXv7hk/hotel12.jpg" alt="" className="gallery-img-m"/></div></div>
-                <div className="slide fade"><div className="gallery-img-container6 galimgmob"><img src="https://i.ibb.co/0sVhTLP/hotel13.jpg" alt="" className="gallery-img-m"/></div></div>
-                <div className="slide fade"><div className="gallery-img-container7 galimgmob"><img src="https://i.ibb.co/N1cwVcR/hotel15.jpg" alt="" className="gallery-img-m" /></div></div>
-                <div className="slide fade"><div className="gallery-img-container8 galimgmob"><img src="https://i.ibb.co/tQ0v8kr/hotel1.jpg" alt="" className="gallery-img-m"/></div></div>
+                <div className="slide fade"><div className="gallery-img-container2 galimgmob"><img src="https://i.postimg.cc/d0tsMXZL/hotel7.jpg" alt="" className="gallery-img-m"/></div></div>
+                <div className="slide fade"><div className="gallery-img-container3 galimgmob"><img src="https://i.postimg.cc/5t7xf38p/hotel8.jpg" alt="" className="gallery-img-m"/></div></div>
+                <div className="slide fade"><div className="gallery-img-container5 galimgmob"><img src="https://i.postimg.cc/BQ9JZt1W/hotel12.jpg" alt="" className="gallery-img-m"/></div></div>
+                <div className="slide fade"><div className="gallery-img-container6 galimgmob"><img src="https://i.postimg.cc/htMn0rZg/hotel13.jpg" alt="" className="gallery-img-m"/></div></div>
+                <div className="slide fade"><div className="gallery-img-container7 galimgmob"><img src="https://i.postimg.cc/JhnLYkSX/hotel15.jpg" alt="" className="gallery-img-m" /></div></div>
+                <div className="slide fade"><div className="gallery-img-container8 galimgmob"><img src="https://i.postimg.cc/7PCLYpsz/hotel1.jpg" alt="" className="gallery-img-m"/></div></div>
                 <div className="slide fade"><div className="gallery-img-container9 galimgmob"><img src="https://i.ibb.co/yBmzbSr/hotel2.jpg" alt="" className="gallery-img-m"/></div></div>
                 <a class="prev" onClick={(e) => plusSlides(-1)}>&#10094;</a>
                 <a class="next" onClick={(e) => plusSlides(1)}>&#10095;</a>
@@ -62,13 +62,13 @@ export const Gallery = () => {
         </div>
             <div className="gallery-wrap">
                 <div className="gallery-img-container galimg"><img src="https://i.ibb.co/3CY1QVB/hotel3.jpg" alt="" className="gallery-img" /></div>
-                <div className="gallery-img-container2 galimg"><img src="https://i.ibb.co/NTyR4yF/hotel7.jpg" alt="" className="gallery-img"/></div>
-                <div className="gallery-img-container3 galimg"><img src="https://i.ibb.co/8Yw8shC/hotel8.jpg" alt="" className="gallery-img"/></div>
-                <div className="gallery-img-container4 galimg"><img src="https://i.ibb.co/c82Q6S3/hotel9.jpg" alt="" className="gallery-img"/></div>
-                <div className="gallery-img-container5 galimg"><img src="https://i.ibb.co/frXv7hk/hotel12.jpg" alt="" className="gallery-img"/></div>
-                <div className="gallery-img-container6 galimg"><img src="https://i.ibb.co/0sVhTLP/hotel13.jpg" alt="" className="gallery-img"/></div>
-                <div className="gallery-img-container7 galimg"><img src="https://i.ibb.co/N1cwVcR/hotel15.jpg" alt="" className="gallery-img" /></div>
-                <div className="gallery-img-container8 galimg"><img src="https://i.ibb.co/tQ0v8kr/hotel1.jpg" alt="" className="gallery-img"/></div>
+                <div className="gallery-img-container2 galimg"><img src="https://i.postimg.cc/d0tsMXZL/hotel7.jpg" alt="" className="gallery-img"/></div>
+                <div className="gallery-img-container3 galimg"><img src="https://i.postimg.cc/5t7xf38p/hotel8.jpg" alt="" className="gallery-img"/></div>
+                <div className="gallery-img-container4 galimg"><img src="https://i.postimg.cc/qqn0XHSZ/hotel9.jpg" alt="" className="gallery-img"/></div>
+                <div className="gallery-img-container5 galimg"><img src="https://i.postimg.cc/BQ9JZt1W/hotel12.jpg" alt="" className="gallery-img"/></div>
+                <div className="gallery-img-container6 galimg"><img src="https://i.postimg.cc/htMn0rZg/hotel13.jpg" alt="" className="gallery-img"/></div>
+                <div className="gallery-img-container7 galimg"><img src="https://i.postimg.cc/JhnLYkSX/hotel15.jpg" alt="" className="gallery-img" /></div>
+                <div className="gallery-img-container8 galimg"><img src="https://i.postimg.cc/7PCLYpsz/hotel1.jpg" alt="" className="gallery-img"/></div>
                 <div className="gallery-img-container9 galimg"><img src="https://i.ibb.co/yBmzbSr/hotel2.jpg" alt="" className="gallery-img"/></div>
             </div>
         </div>
